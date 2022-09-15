6c8f5edae2bca537946b480957b014289c819253
symcetin <seyma.oskurcu@gmail.com>
2022-09-15 14:41:32 -0400

    register form completed

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,21 +4,31 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
-@RequestMapping("mentor")
+@RequestMapping("/mentor")
 public class MentorController {
 
-    @GetMapping("register")
+    @GetMapping("/register")
     public String showForm(Model model) {
         model.addAttribute("mentor",new Mentor());
 
         List<String> batchList = Arrays.asList("JD1", "JD2", "EU1", "EU2", "B18", "B15");
         model.addAttribute("batchList",batchList);
        return "mentor/mentor-register" ;
+    }
+
+    @PostMapping("/confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor) {
+        System.out.println(mentor.toString());
+
+//        return "mentor/mentor-confirmation" ;
+        return "redirect:/mentor/register";
     }
 }
