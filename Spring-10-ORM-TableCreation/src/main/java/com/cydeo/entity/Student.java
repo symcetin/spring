0c8b3e3f235a0012853e0613d9a46fe00f6b8295
symcetin <seyma.oskurcu@gmail.com>
2022-10-13 07:36:01 -0400

    Update table and column names

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,14 +1,18 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "studentFirstName")
     private String firstName; //first_name
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
 }
