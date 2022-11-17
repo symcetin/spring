a4a9954f0de55113c9c000ecad47e12f2ceadbfb
symcetin <seyma.oskurcu@gmail.com>
2022-11-17 14:27:45 -0500

    UserRepository queries completed

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,7 +1,7 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.entity.Account;
-import com.cydeo.repository.AccountRepository;
+import com.cydeo.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +11,26 @@ import java.util.Optional;
 public class DataGenerator implements CommandLineRunner {
 
     private final AccountRepository accountRepository;
+    private final CinemaRepository cinemaRepository;
+    private final GenreRepository genreRepository;
+    private final MovieCinemaRepository movieCinemaRepository;
+    private final MovieRepository movieRepository;
+    private final TicketRepository ticketRepository;
+    private final UserRepository userRepository;
 
-    public DataGenerator(AccountRepository accountRepository) {
+    public DataGenerator(AccountRepository accountRepository, CinemaRepository cinemaRepository, GenreRepository genreRepository, MovieCinemaRepository movieCinemaRepository, MovieRepository movieRepository, TicketRepository ticketRepository, UserRepository userRepository) {
         this.accountRepository = accountRepository;
+        this.cinemaRepository = cinemaRepository;
+        this.genreRepository = genreRepository;
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.movieRepository = movieRepository;
+        this.ticketRepository = ticketRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-
-
+        System.out.println(userRepository.findAllByAccountAgeGreaterThan(40));
 
     }
 }
