c1f96dc52415e3c61064254e4ce39181d7c31f7e
symcetin <seyma.oskurcu@gmail.com>
2022-11-18 10:34:43 -0500

    Data Generator updated

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -12,24 +12,22 @@ public class DataGenerator implements CommandLineRunner {
 
     private final AccountRepository accountRepository;
     private final CinemaRepository cinemaRepository;
-    private final GenreRepository genreRepository;
     private final MovieCinemaRepository movieCinemaRepository;
-    private final MovieRepository movieRepository;
     private final TicketRepository ticketRepository;
     private final UserRepository userRepository;
 
-    public DataGenerator(AccountRepository accountRepository, CinemaRepository cinemaRepository, GenreRepository genreRepository, MovieCinemaRepository movieCinemaRepository, MovieRepository movieRepository, TicketRepository ticketRepository, UserRepository userRepository) {
+    public DataGenerator(AccountRepository accountRepository, CinemaRepository cinemaRepository, MovieCinemaRepository movieCinemaRepository, TicketRepository ticketRepository, UserRepository userRepository) {
         this.accountRepository = accountRepository;
         this.cinemaRepository = cinemaRepository;
-        this.genreRepository = genreRepository;
         this.movieCinemaRepository = movieCinemaRepository;
-        this.movieRepository = movieRepository;
         this.ticketRepository = ticketRepository;
         this.userRepository = userRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println(accountRepository.fetchAdminUsers());
+        System.out.println(ticketRepository.countTicketByUser(3l));
         System.out.println(userRepository.findAllByAccountAgeGreaterThan(40));
 
     }
